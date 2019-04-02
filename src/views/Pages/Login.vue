af93567ec59f9181981c24eb074b7ad8df743cd4
nyagoo <acc.mg47@gmail.com>
2019-04-03 00:01:18 +0900

    [Update] Firebase auth 処理追加

--- src/views/Pages/Login.vue
+++ src/views/Pages/Login.vue
@@ -2,20 +2,40 @@
   .login
     h1 {{headingTitle}}
     v-btn(color="primary" @click="login()") Google Login
+    v-btn(color="primary" @click="logout()") Google Login
+    v-btn(color="primary" @click="loginCheck()") {{isLogin}}
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import router from "@/router";
+import firebase from "firebase";
 
 @Component({
   name: "login",
   components: {}
 })
-export default class Home extends Vue {
+export default class Login extends Vue {
   headingTitle: string = "ログイン";
+  isLogin = false;
   login() {
-    router.push("home");
+    firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider());
+
+    // router.push("home");
+  }
+  logout() {
+    firebase.auth().signOut();
+  }
+  created() {
+    firebase.auth().onAuthStateChanged(user => {
+      // eslint-disable-next-line no-console
+      console.log(user);
+      if (user) {
+        this.isLogin = true;
+      } else {
+        this.isLogin = false;
+      }
+    });
   }
 }
 </script>
